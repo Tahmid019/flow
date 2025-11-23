0250409daedf963e4936ca4415f8c92422134da1
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-11-23 16:35:35 +0530

    upd: url

--- src/app/api/latest_tasks/route.ts
+++ src/app/api/latest_tasks/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const res = await fetch('http://192.168.221.97:8000/latest_tasks/');
+    const res = await fetch('https://flow-8b38.onrender.com/api/latest_tasks/');
     const data = await res.json();
     return NextResponse.json(data);
   } catch (err) {
--- src/app/api/proxy.ts
+++ src/app/api/proxy.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch('http://192.168.221.97:8000/latest_state/');
+    const res = await fetch('https://flow-8b38.onrender.com/api/latest_state/');
     const data = await res.json();
     return new Response(JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' }
